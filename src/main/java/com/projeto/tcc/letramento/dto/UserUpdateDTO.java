7768ae6cd6c6eeeca6dbf2e8237b64b701ae62ea
Igo Quintino <igocastro.15@gmail.com>
2026-05-14 18:48:19 -0300

    feat: add UserUpdateDTO for updating student academic segmentation data

---
+++ src/main/java/com/projeto/tcc/letramento/dto/UserUpdateDTO.java
@@ -0,0 +1,7 @@
+package com.projeto.tcc.letramento.dto;
+
+/**
+ * DTO utilizado para atualizar os dados de segmentação acadêmica do aluno.
+ */
+public record UserUpdateDTO(String course, String academicLevel) {
+}
