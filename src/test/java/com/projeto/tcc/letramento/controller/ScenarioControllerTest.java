736513066099ea4bda7cc22076c3048c3fbae000
Igo Quintino <igocastro.15@gmail.com>
2026-05-21 15:14:05 -0300

    test: restore project-specific jackson-databind; use project WebMvcTest/MockitoBean imports to match runtime

--- src/test/java/com/projeto/tcc/letramento/controller/ScenarioControllerTest.java
+++ src/test/java/com/projeto/tcc/letramento/controller/ScenarioControllerTest.java
@@ -6,10 +6,10 @@ import com.projeto.tcc.letramento.security.JwtTokenProvider;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -29,13 +29,13 @@ class ScenarioControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private ScenarioService scenarioService;
 
-    @MockBean
+    @MockitoBean
     private ProgressService progressService;
 
-    @MockBean
+    @MockitoBean
     private JwtTokenProvider jwtTokenProvider;
 
     @Test
