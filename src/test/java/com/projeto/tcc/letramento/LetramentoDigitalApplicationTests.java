35daaaf73d78207bb9923e35cceb78a6885aebbf
Igo Quintino <igocastro.15@gmail.com>
2026-05-27 17:52:57 -0300

    Test: método contextLoad funcionando

--- src/test/java/com/projeto/tcc/letramento/LetramentoDigitalApplicationTests.java
+++ src/test/java/com/projeto/tcc/letramento/LetramentoDigitalApplicationTests.java
@@ -3,9 +3,14 @@ package com.projeto.tcc.letramento;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+		"spring.security.oauth2.client.registration.google.client-id=mock-google-id",
+		"spring.security.oauth2.client.registration.google.client-secret=mock-google-secret"
+})
 @Disabled("placeholder")
+@ActiveProfiles("test")
 class LetramentoDigitalApplicationTests {
 
 	@Test
